14c361831f5154eebb75a1d1d9d1a7b9744bcbee
mathieu <hei.mathieu.22@gmail.com>
2025-08-26 22:56:17 +0300

    feat: Ajout des route possible

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Auth from './pages/auth/Auth';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
+import Header from './components/layout/Header';
 
 function App() {
   return (
@@ -12,6 +13,14 @@ function App() {
           <Route path="/login" element={<Auth mode="login" />} />
           <Route path="/signup" element={<Auth mode="signup" />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/expenses" element={<></>} />
+          <Route path="/expenses/new" element={<></>} />
+          <Route path="/expenses/:id/edit" element={<></>} />
+          <Route path="/incomes" element={<></>} />
+          <Route path="/incomes/new" element={<></>} />
+          <Route path="/categories" element={<></>} />
+          <Route path="/profile" element={<></>} />
+          <Route path="/receipts/:idExpense" element={<></>} />
         </Routes>
       </div>
     </Router>
