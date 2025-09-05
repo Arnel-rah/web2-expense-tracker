2e6cee77b364975eca7f6da048c8984a7daf66bd
Tsiry <hei.tsiry@gmail.com>
2025-09-05 14:32:24 +0300

    feat: add style in transction list

--- frontend/src/components/ui/TransactionsListe.tsx
+++ frontend/src/components/ui/TransactionsListe.tsx
@@ -1,50 +1,73 @@
-import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
-export default function List({ data }: { data: any[] }) {
-  const columnsToShow = ['description', 'amount', 'date', 'type'];
-  const keysToDisplay = Object.keys(data[0] || {}).filter((key) => columnsToShow.includes(key));
-  const [filter, setFilter] = useState<string>('all');
-
-  const filteredData = filter === 'all' ? data : data.filter((row) => row.type === filter);
+export default function List({ data, transaction }: { data: any[], transaction : string}) {
+  const columnsToShow = ['title', 'amount', 'date', 'category'];
+  const keysToDisplay = Object.keys(data[0] || {}).filter((key) =>
+    columnsToShow.includes(key)
+  );
 
   return (
-    <div>
-      <h1>Liste des Transactions</h1>
-      
-      <div className='flex justify-around'>
-        <button onClick={() => setFilter('all')}>
-          All
-        </button>
-        <button onClick={() => setFilter('income')}>
-          Incomes
-        </button>
-        <button onClick={() => setFilter('expense')}>
-          Expenses
-        </button>
-      </div>
+    <div className="max-w-5xl mx-auto mt-10 p-4 bg-white shadow-lg rounded-md">
+      <h1 className="text-2xl font-bold mb-6 text-gray-800 text-center">
+{
+  transaction == "income"?"Income List": "Expense List"
+}
 
-      <table>
-        <thead>
-          <tr>
-            {keysToDisplay.map((title) => (
-              <th key={title}>
-                {title}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {keysToDisplay.map((value, colIndex) => (
-                <td key={colIndex}>
-                  {row[value]}
-                </td>
+
+      </h1>
+
+      <div className="overflow-x-auto">
+        <table className="min-w-full border border-gray-200">
+          <thead className="bg-gray-200 min-w-full">
+            <tr>
+              {keysToDisplay.map((title) => (
+                <th
+                  key={title}
+                  className="px-6 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider border-b"
+                >
+                  {title}
+                </th>
               ))}
+              <th className="text-center border-b">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+                className={rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-100 whitespace-nowrap border-b'}
+              >
+                {keysToDisplay.map((value, colIndex) => (
+                  <td
+                    key={colIndex}
+                    className="text-sm px-6 py-4 text-gray-800 "
+                  >
+                    {row[value]}
+                  </td>
+                ))}
+                <td className="text-center">
+                  <button
+                    // onClick={() => handleEdit(row)}
+                    className="text-blue-500 hover:text-blue-700 mr-2"
+                    title="Edit"
+                  >
+                    <FontAwesomeIcon icon={faPenToSquare} />
+                  </button>
+
+                  <button
+                    // onClick={() => handleDelete(row.id)}
+                    className="text-red-500 hover:text-red-700"
+                    title="Delete"
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
