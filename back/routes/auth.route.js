ddbb51695a3042d02e9ee5d8067dda7d2317ae46
Tsiry <hei.tsiry@gmail.com>
2025-09-11 11:22:21 +0300

    feat: add method patch

--- back/routes/auth.route.js
+++ back/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login} from '../controllers/authController.js';
+import { signup, login, changePassword} from '../controllers/authController.js';
 import { logRequest } from '../middlewares/example.js';
 import { authenticate } from '../middlewares/auth.js';
 import { getProfile } from '../controllers/profileController.js';
@@ -9,5 +9,6 @@ router.use(logRequest);
 router.post('/signup', signup);//veifié
 router.post('/login', login);//verifié
 router.get('/me', authenticate, getProfile);//verifié
+router.patch('/change-password', authenticate, changePassword)
 
 export default router;
